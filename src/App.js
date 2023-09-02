e7fe0c57ce222591a3155a8439b60e853d6e149b
kiatklai <likhitthaworn.k@gmail.com>
2023-09-02 22:40:05 +0900

    ページ設定

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 import FoodComponent from "./components/FoodComponent";
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import MenuData from "./data/MenuData";
 import './App.css'
 
 function App() {
   const [foodData,setFoodData] = useState(MenuData)
+  const [dataInPage,setDataInPage] = useState([])
+
+  const pagination=()=>{
+    const foodPerPage = 5
+    const pages = Math.ceil(MenuData.length / foodPerPage)
+    console.log("page number =",pages)
+
+    const newFood=Array.from({length:pages},(data,index)=>{
+      const start = index * foodPerPage
+      return MenuData.slice(start,start+foodPerPage)
+    })
+    return newFood
+  }
+
+  useEffect(()=>{
+    const paginate=pagination()
+    setDataInPage(paginate)
+  },[])
+
   return (
     <div className="App">
       <h1>FoodCard | Pagination</h1>
       <div className="container">
         {foodData.map((data,index)=>{
           return <FoodComponent key={index} {...data}/>
+        })}
+      </div>
+      <div className="pagination-container">
+        {dataInPage.map((data,index)=>{
+          return(
+            <button>{index+1}</button>
+          )
         })}
       </div>
     </div>
